c93602b334ff27a433ec77b1f9767cc52539089d
Arzoo Khan <arzookhan2099@gmail.com>
2025-04-24 14:58:43 +0530

    JFDS 914
    frame width calculation fixed

--- resources/views/Items/Vicaima/VicaimaDoorModules/Frame.blade.php
+++ resources/views/Items/Vicaima/VicaimaDoorModules/Frame.blade.php
@@ -100,7 +100,7 @@
                                                     @endif
                                                     </label>
                                                     {{--  <label for="Rebated_Height" style="display: none;">Rebated Height</label>  --}}
-                                                    <input type="number" @if(empty($Item['ScallopedHeight'])){{'readonly'}}@endif min="12" name="ScallopedHeight" id="ScallopedHeight" class="form-control"
+                                                    <input type="number" @if(empty($Item['ScallopedHeight'])){{'readonly'}}@endif min="12" name="ScallopedHeight" id="ScallopedHeight" class="form-control change-event-calulation"
                                                     value="@if(isset($Item['ScallopedHeight'])){{$Item['ScallopedHeight']}}@else{{'0'}}@endif">
                                                 </div>
                                             </div>
