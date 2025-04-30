126f6bfa5ed357c716c7f62596b29f076dd6df0f
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-04-30 12:15:18 +0530

    update

--- public/vicaima/vicaima-custome-rules.js
+++ public/vicaima/vicaima-custome-rules.js
@@ -5753,7 +5753,6 @@ function UpdateformulaSoHeightAndWidth(val){
         console.log(parseInt(leafHeightNoOP) , parseInt(adjustmentLeafHeightNoOP) , parseInt(gap) , parseInt(undercut) , parseInt(frame_thickness) ,  parseInt(tollerance) , parseInt(oPHeigth), 'soheight', soHeight)
         $("#sOWidth").val(so_width)
         $("#sOHeight").val(soHeight);
-        frameHeight();
     }
 
 }
@@ -5817,7 +5816,6 @@ function updateformulaS1(val){
 
         $("#sOWidth").val(so_width)
         $("#sOHeight").val(soHeight);
-        frameHeight();
     }
 
 }
@@ -5880,7 +5878,6 @@ function updateformulaS2(val){
 
         $("#sOWidth").val(so_width)
         $("#sOHeight").val(soHeight);
-        frameHeight();
     }
 
 }
