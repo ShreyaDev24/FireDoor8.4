4b5548defb4bb7f1701927a5f9a59a66ed842ee1
Arzoo Khan <arzookhan2099@gmail.com>
2025-05-06 13:59:22 +0530

    JFDS 869
    Vp issue fixed

--- public/vicaima/vicaima-custome-rules.js
+++ public/vicaima/vicaima-custome-rules.js
@@ -474,10 +474,10 @@ $("#leaf1VisionPanel").change(function () {
         $('#glassThickness').attr('required', true);
         $('#glazingBeads').attr('required', true);
         $('#glazingBeadsThickness').attr('required', true);
-        $('#glazingBeadsWidth').attr('required', true);
+        // $('#glazingBeadsWidth').attr('required', true);
         $('#glazingBeadsHeight').attr('required', true);
         $('#glazingBeadsFixingDetail').attr('required', true);
-        $('#glazingBeadSpecies').attr('required', true);
+        // $('#glazingBeadSpecies').attr('required', true);
         doorLeafFacingPrice('leaf1VisionPanel', 'Yes');
         doorLeafFacingPrice('leaf1VisionPanel1', 'Yes');
     } else {
@@ -499,7 +499,7 @@ $("#leaf1VisionPanel").change(function () {
         $('#glassType').val('').attr('required', false);
         $('#glazingBeads').val('').attr('required', false);
         $('#glazingBeadsThickness').val('').attr('required', false);
-        $('#glazingBeadsWidth').val('').attr('required', false);
+        // $('#glazingBeadsWidth').val('').attr('required', false);
         $('#glazingBeadsHeight').val('').attr('required', false);
         $('#glazingBeadsFixingDetail').val('').attr('required', false);
         $('#glazingBeadSpecies').val('').attr('required', false);
