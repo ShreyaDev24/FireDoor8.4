5c1acad01c573579e04b8f3351df1c457f3cb6b4
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-06-03 17:09:47 +0530

    JFDS-983

--- public/Flamebreak/flamebreak-custome-rules.js
+++ public/Flamebreak/flamebreak-custome-rules.js
@@ -89,12 +89,6 @@ function pageIdentity(){
             let framTypeValue = $('#frameType').val();
             if (framTypeValue == "Plant_on_Stop" || framTypeValue == "Rebated_Frame") {
                 $("#frameThickness").attr('min','30');
-            }else{
-                if($("#swingType").val() == "SA"){
-                    $("#frameThickness").attr('min','32');
-                }else if($("#swingType").val() == "DA"){
-                    $("#frameThickness").attr('min','37');
-                }
             }
         }
 
@@ -858,6 +852,7 @@ $(document).ready(function() {
             $("#plantonStopWidth-section,#plantonStopHeight-section,#ScallopedWidth-section,#ScallopedHeight-section,#rebatedWidth-section,#rebatedHeight-section").removeClass("table_row_show");
             $("#plantonStopWidth-section,#plantonStopHeight-section,#ScallopedWidth-section,#ScallopedHeight-section,#rebatedWidth-section,#rebatedHeight-section").addClass("table_row_hide");
         }
+        frameThicknessChange();
     }
 
     $(document).on('change','#frameDepth',function(e){
