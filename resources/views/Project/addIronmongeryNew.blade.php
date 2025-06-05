729742122f09588e7858c5671a8d4ca2b6428c44
Arzoo Khan <arzookhan2099@gmail.com>
2025-06-05 11:33:38 +0530

    ironmongary issue fixed

--- resources/views/Project/addIronmongeryNew.blade.php
+++ resources/views/Project/addIronmongeryNew.blade.php
@@ -426,9 +426,29 @@ input[type=number]::-webkit-outer-spin-button {
         // })
     })
 
+    // async function removeMe(dis) {
+    //     let id = $(dis).attr('data-id');
+    //     $(`#${id}`).remove();
+    //     setTimeout(() => {
+    //         syncRequire($(dis).attr('data-main-id'));
+    //         TotalPrice();
+    //     }, 500);
+    // }
+
     async function removeMe(dis) {
         let id = $(dis).attr('data-id');
-        $(`#${id}`).remove();
+        // Normalize ID casing: convert "LocksAndLatches" to "LocksandLatches"
+        id = id.replace('LocksAndLatches', 'LocksandLatches');
+        id = id.replace('DoorSinage', 'DoorSignage');
+        id = id.replace('AirTransferGrill', 'Airtransfergrills');
+        id = id.replace('FaceFixedDoorCloser', 'FaceFixedDoorClosers');
+        id = id.replace('KeyholeEscutchen', 'KeyholeEscutcheon');
+        const target = document.getElementById(id);
+        if (target) {
+            target.remove();
+        } else {
+            console.warn(`Element with ID '${id}' not found`);
+        }
         setTimeout(() => {
             syncRequire($(dis).attr('data-main-id'));
             TotalPrice();
