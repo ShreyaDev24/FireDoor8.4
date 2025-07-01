6041a9f723bd7994d332e33ce4e873f7a5efca44
Arzoo Khan <arzookhan2099@gmail.com>
2025-07-01 18:33:27 +0530

    JFDS 1039
    done

--- app/Http/Controllers/DoorScheduleController.php
+++ app/Http/Controllers/DoorScheduleController.php
@@ -2087,6 +2087,7 @@ class DoorScheduleController extends Controller
                     $SlBeadHeight = trim((string) $row[$j++]);
                     $SL1Depth = trim((string) $row[$j++]);
                     $SL1Transom = trim((string) $row[$j++]);
+                    $test2 = trim((string) $row[$j++]);
                     $SideLight2 = trim((string) $row[$j++]);
                     $DoYouWantToCopySameAsSL1 = trim((string) $row[$j++]);
                     $SideLight2GlassType = trim((string) $row[$j++]);
@@ -9123,15 +9124,16 @@ class DoorScheduleController extends Controller
             $rules['OPGlazingBeadsThickness'] = 'required';
             $rules['OPGlazingBeadsHeight'] = 'required';
             $rules['OPGlazingBeadsFixingDetail'] = 'required';
-            $rules['OPGlazingBeadSpecies'] = 'required';
         }
 
         // Optional Section: Lipping
         if (!empty($door['FireRating'])) {
-            $rules['IntumescentLeapingSealType'] = 'required';
-            $rules['IntumescentLeapingSealLocation'] = 'required';
-            $rules['IntumescentLeapingSealColor'] = 'required';
-            $rules['IntumescentLeapingSealArrangement'] = 'required';
+            if($door['FireRating'] != "NFR"){
+                $rules['IntumescentLeapingSealType'] = 'required';
+                $rules['IntumescentLeapingSealLocation'] = 'required';
+                $rules['IntumescentLeapingSealColor'] = 'required';
+                $rules['IntumescentLeapingSealArrangement'] = 'required';
+            }
         }
 
          if ($door['accoustics'] == 'Yes') {
