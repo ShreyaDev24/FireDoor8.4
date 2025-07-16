ba76fa839318fad2ca4da2e90729a512bbe078f4
Arzoo Khan <arzookhan2099@gmail.com>
2025-07-16 16:04:54 +0530

    JFDS 1049
    done

--- app/Exports/BomCalculationScreenExport.php
+++ app/Exports/BomCalculationScreenExport.php
@@ -17,6 +17,8 @@ class BomCalculationScreenExport implements WithMultipleSheets
     {
         return [
             'Screen Summary' => new ScreenSummary($this->id,$this->vid,$this->result),
+            'Side Screens Glazing Beads' => new SideScreenGlazingBeads($this->id,$this->vid,$this->result),
+            'Side Screens Frame' => new SideScreenFrame($this->id,$this->vid,$this->result),
             'Side Screens Glass' => new SideScreenGlass($this->id,$this->vid,$this->result),
             'Side Screens Glazing' => new SideScreenGlazing($this->id,$this->vid,$this->result),
             'Side Screens General Labour Costs' => new SideScreenGeneralLabourCosts($this->id,$this->vid,$this->result),
