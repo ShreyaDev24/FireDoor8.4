5f0d0e2eca06b8ffcb73d7af0665f5e5c60c4443
Arzoo Khan <arzookhan2099@gmail.com>
2025-07-21 17:23:09 +0530

    JFDS 1059
    For Streboard its done

--- public/js/change-event-calculation.js
+++ public/js/change-event-calculation.js
@@ -118,6 +118,27 @@ $(".change-event-calulation").change(function(){
 
         }
 
+        //get value of frame Width from field
+        if(thisvalue[i].name=='frameWidth'){
+            if(thisvalue[i].value==''){
+                FrameWidth = 0;
+            }
+            else{
+                FrameWidth = thisvalue[i].value;
+            }
+
+        }
+        //get value of framethickness from field
+        if(thisvalue[i].name=='OpBeadThickness'){
+            if(thisvalue[i].value==''){
+                OpBeadThickness = 0;
+            }
+            else{
+                OpBeadThickness = thisvalue[i].value;
+            }
+
+        }
+
         //get value of SO Height Width from field
         if(thisvalue[i].name=='sOHeight'){
             if(thisvalue[i].value==''){
@@ -346,8 +367,12 @@ $(".change-event-calulation").change(function(){
             }
         }
     }
-    var calculateOfOpWidth = soWidth-(tollerance*TolleranceAdditionalNumberForOPWidth)-(framethikness*FrameThicknessAdditionalNumberForOPWidth)-(GapAdditionalNumberForOPWidth*gap);
+    // old calculation
+    // var calculateOfOpWidth = soWidth-(tollerance*TolleranceAdditionalNumberForOPWidth)-(framethikness*FrameThicknessAdditionalNumberForOPWidth)-(GapAdditionalNumberForOPWidth*gap);
+    // new calculation according to 1059 => OP/FL width = Frame width - OP/Fl frame thickness X2.
+    var calculateOfOpWidth = FrameWidth - (OpBeadThickness * 2);
     $("#oPWidth").val(calculateOfOpWidth);
+    //end
 
     // Leaf width 1 and leaf width 2 calculation according to doorsetType
 
