9a40366a2cf9d06ae53df6155f40095d483268c2
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-08-29 11:14:27 +0530

    JFDS-1062

--- resources/views/favorites/show.blade.php
+++ resources/views/favorites/show.blade.php
@@ -12,4 +12,36 @@
         {!! $html !!}
     </div>
 </div>
+<input type="hidden" id="favoriteDeleteItem" value="{{ url('/quotation/favoriteDeleteItem') }}" />
+    <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}" />
+@endsection
+@section('js')
+    <script>
+        function favoriteDeleteItem(id) {
+            $('.loader').empty().css({
+                'display': 'block'
+            });
+            var r = confirm("Are you sure! you wan't to delete it.");
+            if (r == true) {
+                $.ajax({
+                    url: $("#favoriteDeleteItem").val(),
+                    method: "POST",
+                    data: {
+                        _token: $("#_token").val(),
+                        id: id
+                    },
+                    dataType: "Json",
+                    success: function(data) {
+                        if (data.status == true) {
+                            location.reload();
+                        } else {
+                            swal('error', data.msg, 'error').then(function() {
+                                location.reload();
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    </script>
 @endsection
