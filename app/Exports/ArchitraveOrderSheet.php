9b5a989f0b7c831491b1c9373e7f3f2e9d5fc260
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-09-17 11:53:17 +0530

    JFDS-1112

--- app/Exports/ArchitraveOrderSheet.php
+++ app/Exports/ArchitraveOrderSheet.php
@@ -92,7 +92,7 @@ class ArchitraveOrderSheet implements FromCollection,WithHeadings,WithEvents,Wit
                     $SpeciesName,
                     $value->ArchitraveFinish,
                     $value->ArchitraveSetQty,
-                    (($FrameHeight * 2) + $FrameWidth + $OPHeight +  $SL1Width + $SL2Width)/1000, // LM Per Door Type
+                    ((($FrameHeight + $OPHeight) * 2) + $FrameWidth  +  $SL1Width + $SL2Width)/1000, // LM Per Door Type
                     $totalHeight, // Leg x2
                     $totalWidth  // Head
                 ];
