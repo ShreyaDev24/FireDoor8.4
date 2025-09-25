34b514c826142578ca6036a8f6db30c82ea52108
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-09-25 12:43:26 +0530

    JFDS-1118

--- resources/views/Order/pdf_files/labels.blade.php
+++ resources/views/Order/pdf_files/labels.blade.php
@@ -3,16 +3,31 @@
 <head>
     <title>Labels</title>
     <style>
-        body { font-family: Arial, sans-serif; font-size: 14px; }
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 11pt;
+        }
+
         .label {
+            display: inline-block;
+            vertical-align: top;
             border: 1px solid #000;
-            padding: 10px;
-            margin-bottom: 15px;
-            width: 300px;
+            padding: 4mm;
+            margin: 2mm;
+            width: 60mm;              /* Fixed width */
+            height: 70mm;             /* Fixed height to ensure uniformity */
+            box-sizing: border-box;   /* Includes padding in width/height */
+            overflow: hidden;         /* Prevent content from expanding box */
+            page-break-inside: avoid;
+        }
+
+        @page {
+            size: A4 portrait;
+            margin: 10mm;
         }
     </style>
 </head>
 <body>
-     {!! $html !!}
+    {!! $html !!}
 </body>
 </html>
