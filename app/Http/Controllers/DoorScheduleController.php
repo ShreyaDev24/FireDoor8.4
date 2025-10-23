b8d63c83dc69c61491f25c90ab64130b40d5aa0e
Arzoo Khan <arzookhan2099@gmail.com>
2025-10-23 13:09:48 +0530

    add logs for site not working

--- app/Http/Controllers/DoorScheduleController.php
+++ app/Http/Controllers/DoorScheduleController.php
@@ -4131,6 +4131,7 @@ class DoorScheduleController extends Controller
 
     public function generateQuotation(string $Id, string $vId, $pId = null, $cId = null)
     {
+        \DB::enableQueryLog();
         markAsRead($Id, 'quote');
         if ($Id == 0 && $vId == 0) {
             $qidFromhelper = GenerateQuotationFirstTime($pId, $cId);
@@ -4520,6 +4521,7 @@ class DoorScheduleController extends Controller
             }
 
             $favorites = Favorite::with('user')->where(['userId'=>Auth::id(),'status'=>1])->latest()->get();
+            \Log::info('Quotation Queries', \DB::getQueryLog());
 
             return view('DoorSchedule.GenerateQuotation', [
                 'data' => $Schedule,
