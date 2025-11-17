8c162c0748813496826019c7d0bb88e08ea40d45
Arzoo Khan <arzookhan2099@gmail.com>
2025-11-17 11:54:12 +0530

    syntax error

--- app/Http/Controllers/DoorScheduleController.php
+++ app/Http/Controllers/DoorScheduleController.php
@@ -10135,6 +10135,7 @@ public function previewAdjustPriceDiscount(Request $request)
         Log::error("Preview Adjust Price ERROR: ".$e->getMessage());
         return response()->json(['status'=>false,'msg'=>$e->getMessage()]);
     }
+}
 
     public function updateRevision(Request $request){
         $url = url('quotation/generate') . '/' . $request->quotationId . '/' . $request->version;
