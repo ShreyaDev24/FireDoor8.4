fb0acd1fb895595c415ef9cd71dada14cad454d0
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-11-21 17:26:37 +0530

    JFDS-1165

--- app/Http/Controllers/PrintInvoiceController.php
+++ app/Http/Controllers/PrintInvoiceController.php
@@ -1006,8 +1006,8 @@ class PrintInvoiceController extends Controller
                     'FD60' => 'Chilt/A02067 Revision M',
                 ],
                 2 => [
-                    'NFR' => 'Chilt/A01204 Revision H',
-                    'FD30' => 'Chilt/A01204 Revision H',
+                    'NFR' => 'Chilt/A01205 Part 1 Revision K',
+                    'FD30' => 'Chilt/A01205 Part 1 Revision K',
                     'FD60' => 'FEA/F96103  Revision Q',
                 ],
                 7 => [
@@ -4796,7 +4796,7 @@ if($tt->DoorsetType == "SD" &&  $tt->FrameType==null ){
                     'FD60' => 'Chilt/A02067 Revision M',
                 ],
                 2 => [
-                    'FD30' => 'Chilt/A01204 Revision H',
+                    'FD30' => 'Chilt/A01205 Part 1 Revision K',
                     'FD60' => 'FEA/F96103  Revision Q',
                 ],
                 7 => [
