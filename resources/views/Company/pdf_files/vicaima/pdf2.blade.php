999bc5f5c4d3cc53e06891a6877a133e974a4d73
ShreyaDev24 <shreyaguptadev24@gmail.com>
2025-11-25 11:55:51 +0530

    quote issue

--- resources/views/Company/pdf_files/vicaima/pdf2.blade.php
+++ resources/views/Company/pdf_files/vicaima/pdf2.blade.php
@@ -215,12 +215,9 @@
                 <th class="page2_2"><p> rW Db Rating </p></th>
                 <th class="page2_2"><p> Fire Rating </p></th>
                 <th class="page2_2"><p> Special Feature Refs </p></th>
-                @if($HideCosts == 0 || $HideCosts != 5)
+                @if($HideCosts == 0)
                 <th class="page2_2 tbl_last"><p> Doorset Price </p></th>
                 <th class="page2_2 tbl_last"><p> Ironmongery Price </p></th>
-                @endif
-                @if($HideCosts != 5)
-                <th class="page2_2 tbl_last"><p> Total Price Per Doorset </p></th>
                 @endif
             </tr>
 
