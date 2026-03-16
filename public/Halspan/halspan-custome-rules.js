220478f50bac4c0376f5ff99843dbaab1ae590e4
Arzoo Khan <arzookhan2099@gmail.com>
2026-03-16 16:22:35 +0530

    glassthicknessissue fixed

--- public/Halspan/halspan-custome-rules.js
+++ public/Halspan/halspan-custome-rules.js
@@ -5214,13 +5214,13 @@ function glass_glazing_system(isstatus = false){
                     }else{
                         glassTypeInnerHtml+='<option value="'+data[i].Key+'">'+data[i].GlassType +'</option>';
                     }
-                    $("#glassThickness").val(data[i].GlassThickness);
+                    // console.log(data[i].GlassThickness);
+                    // $("#glassThickness").val(data[i].GlassThickness);
                 }
             } else {
                 glassTypeInnerHtml += '<option value="">No Glass Type Found</option>';
             }
             $("#glassType").empty().append(glassTypeInnerHtml);
-            // $("#glassThickness").val(0);
         }
     });
 }
@@ -5276,7 +5276,7 @@ function glazing_system(isIntegrity,isstatus = false){
                     }else{
                         glazingSystemInnerHtml+='<option value="'+data[i].Key+'">'+data[i].GlazingSystem +'</option>';
                     }
-                    $("#glassThickness").val(data[i].GlassThickness);
+                    // $("#glassThickness").val(data[i].GlassThickness);
                 }
             } else {
                 glazingSystemInnerHtml += '<option value="">No Glazing Systems Found</option>';
