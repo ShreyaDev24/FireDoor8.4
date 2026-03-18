7409e7aa47af3460542f443b15d119c04c76ab70
Arzoo Khan <arzookhan2099@gmail.com>
2026-03-18 12:14:15 +0530

    JFDS 1197
    debug

--- resources/views/Company/pdf_files/pdf2.blade.php
+++ resources/views/Company/pdf_files/pdf2.blade.php
@@ -216,7 +216,7 @@
                 <th class="page2_2"><p> COC Type </p></th>
                 <th class="page2_2"><p> Special Feature Refs </p></th>
                 @if($HideCosts == 0)
-                <th class="page2_2 tbl_last"><p> Doorset Price </p></th>
+                <th class="page2_2 tbl_last"><p> Doorset Price {{ $HideCosts }} </p></th>
                 <th class="page2_2 tbl_last"><p> Ironmongery Price </p></th>
                 @endif
                 <th class="page2_2 tbl_last"><p> Total Price Per Doorset </p></th>
