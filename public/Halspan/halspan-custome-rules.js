9f2575e8ac6f9c0fc2b2dcd57ff515d51d6353c7
Arzoo Khan <arzookhan2099@gmail.com>
2026-04-13 18:33:20 +0530

    JFDS 1209

--- public/Halspan/halspan-custome-rules.js
+++ public/Halspan/halspan-custome-rules.js
@@ -117,6 +117,10 @@ function pageIdentity(){
         e.preventDefault();
         IntumescentSeals();
     });
+    $(document).on('change','#intumescentSealType',function(e){
+        e.preventDefault();
+        IntumescentSeals();
+    });
     $(document).on('change','#sODepth',function(e){
         e.preventDefault();
         const soDepthValue2 = parseInt($(this).val());
@@ -2990,6 +2994,7 @@ $(document).ready(function() {
         const overpanelValue2 = $('#overpanel').val();     // Yes
         const leafWidth1Value = $('#leafWidth1').val();
         const leafHeightNoOPValue = $('#leafHeightNoOP').val();
+        const intumescentSealType = $('#intumescentSealType').val();
         var sOWidthValue = $('#sOWidth').val();
         var sOHeightValue = $('#sOHeight').val();
         let overpanel = '';
@@ -3025,12 +3030,12 @@ $(document).ready(function() {
             }
         }
         // console.log($aa);
-        if(fireRatingValue != '' && sOWidthValue != '' && sOHeightValue != ''){
+        if(fireRatingValue != '' && sOWidthValue != '' && sOHeightValue != '' || intumescentSealType != ''){
             $.ajax({
                 url: $("#Filterintumescentseals").text(),
                 method:"POST",
                 dataType:"Json",
-                data:{pageId:pageId,SelectedValue:SelectedValue,fireRatingValue:fireRatingValue,intumescentseals: $aa ,leafWidth1Value:leafWidth1Value, leafHeightNoOPValue:leafHeightNoOPValue,doorLeafFacingValueNew:doorLeafFacingValueNew,frameMaterialNew:frameMaterialNew,intumescentsealsleaftype:intumescentsealsleaftype, _token:$("#_token").val()},
+                data:{pageId:pageId,SelectedValue:SelectedValue,fireRatingValue:fireRatingValue,intumescentseals: $aa ,leafWidth1Value:leafWidth1Value, leafHeightNoOPValue:leafHeightNoOPValue,doorLeafFacingValueNew:doorLeafFacingValueNew,frameMaterialNew:frameMaterialNew,intumescentsealsleaftype:intumescentsealsleaftype,intumescentSealType:intumescentSealType ,_token:$("#_token").val()},
                 success: function(result){
                     // console.log(result);
                     // console.log(result.data);
@@ -3061,6 +3066,14 @@ $(document).ready(function() {
             });
         }
     }
+    $('#intumescentSealArrangement').on('change', function () {
+
+        let selectedOption = $(this).find(':selected');
+
+        let fireTested = selectedOption.data('firetested');
+
+        $('#fireratedtestes').val(fireTested || '');
+    });
     function DoorLeafFacingChange(status = false,isstatus = false){
         let pageId = pageIdentity();
         var doorLeafFacing =  $("#doorLeafFacing").val();
