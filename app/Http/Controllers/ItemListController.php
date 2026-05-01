93d405c055201fe3795190fb0e6f83a83ccf834a
ShreyaDev24 <shreyaguptadev24@gmail.com>
2026-05-01 17:51:08 +0530

    JFDS-1128

--- app/Http/Controllers/ItemListController.php
+++ app/Http/Controllers/ItemListController.php
@@ -2464,7 +2464,7 @@ class ItemListController extends Controller
         ->join('glass_type', 'glass_type.id', 'glass_glazing_system.glass_id')
         ->leftJoin('selected_glass_type', 'selected_glass_type.glass_id', 'glass_glazing_system.glass_id')
         ->where('glass_type.GlassIntegrity', $integrity)
-        ->where('glass_type.Configurableitems', $pageId)
+        ->where('glass_type.' . $configurationDoor, $pageId)
         ->whereIn('glass_type.EditBy', $userIds)
         ->where('selected_glass_type.editBy', Auth::user()->id);
 
