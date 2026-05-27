268b76c038ae9ff6bbc9c1c7c51910e0e7ab9de4
Arzoo Khan <arzookhan2099@gmail.com>
2026-05-27 17:20:28 +0530

    JFDS 1208
    for all custom door done

--- resources/views/Items/Stredor/StredorDoorModules/Frame.blade.php
+++ resources/views/Items/Stredor/StredorDoorModules/Frame.blade.php
@@ -24,6 +24,19 @@
                         </div>
 
                     </div>
+                     <div class="col-md-6 framehideshow">
+                        <div class="position-relative form-group">
+                            <label for="frameThickness">Frame Thickness
+                                @if(!empty($tooltip->frameThickness))
+                                <script type="text/javascript">
+                                document.write(Tooltip('{{$tooltip->frameThickness}}'));
+                                </script>
+                                @endif
+                            </label>
+                            <input type="number" id="frameThickness" name="frameThickness" value="@if(isset($Item["FrameThickness"])){{$Item["FrameThickness"]}}@else{{''}}@endif"
+                                class="form-control change-event-calulation door-configuration" required pattern="\d*" maxlength="5" oninput="if(this.value.length > 5) this.value = this.value.slice(0, 5);">
+                        </div>
+                    </div>
                     <div class="col-md-6">
                         <div class="position-relative form-group">
                             <label for="frameType">Frame Type
