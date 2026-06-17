6771230344d8d494fe339feb9362116a76a7c7e8
ShreyaDev24 <shreyaguptadev24@gmail.com>
2026-06-17 13:04:03 +0530

    update

--- public/js/custome-rules.js
+++ public/js/custome-rules.js
@@ -3057,7 +3057,7 @@ function copyOfSideLite1Change(isstatus = false){
                 url: $("#Filterintumescentseals").text(),
                 method:"POST",
                 dataType:"Json",
-                data:{pageId:pageId,SelectedValue:SelectedValue,fireRatingValue:fireRatingValue,intumescentseals: $aa ,leafWidth1Value:leafWidth1Value, leafHeightNoOPValue:leafHeightNoOPValue,doorLeafFacingValueNew:doorLeafFacingValueNew,frameMaterialNew:frameMaterialNew,intumescentsealsleaftype:intumescentsealsleaftype, _token:$("#_token").val()},
+                data:{pageId:pageId,SelectedValue:SelectedValue,fireRatingValue:fireRatin  ,intumescentseals: $aa ,leafWidth1Value:leafWidth1Value, leafHeightNoOPValue:leafHeightNoOPValue,doorLeafFacingValueNew:doorLeafFacingValueNew,frameMaterialNew:frameMaterialNew,intumescentsealsleaftype:intumescentsealsleaftype, _token:$("#_token").val()},
                 success: function(result){
                     // console.log(result);
                     // console.log(result.data);
